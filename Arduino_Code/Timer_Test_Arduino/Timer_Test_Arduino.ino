0238f28cae99cbb80f0be30e28556429f71014cf
JesusNoelValMo <jesusnoel.valdibiezomora@outlook.com>
2019-08-09 11:21:26 -0600

    Se agrego escritura hacia la tablet con valores de los contadores por medio de un timer individual, se escribira el valor que se tiene en el momento

--- Arduino_Code/Timer_Test_Arduino/Timer_Test_Arduino.ino
+++ Arduino_Code/Timer_Test_Arduino/Timer_Test_Arduino.ino
@@ -8,6 +8,7 @@ int Trigger_Test_Pin_2 = 3;
 int Group1_PinTimerDev[11] = {22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32};
 int Group2_PinTimerDev[11] = {33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43};
 String ID_Char[22] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v"};
+String Msg_To_Android = "";
 //Se crea la clase timer_divice, aqui se va a hacer la suma de los tiempos en los que seactivan los timers
 class Timer_device
 {
@@ -16,7 +17,7 @@ class Timer_device
     boolean Pin_Timer_Status;
     unsigned long NextTimeToCheck = 0;
   public:
-    int count;
+    int count = 0;
     boolean is_finished;
     void Initialize_Pin(byte Pin_Timer)
     {
@@ -29,31 +30,26 @@ class Timer_device
       if (currentTime >= NextTimeToCheck)
       {
         NextTimeToCheck = TimeToCheck + currentTime;
-       Serial.println(currentTime);
-       Serial.println(NextTimeToCheck);
+    //   Serial.println(currentTime);
+      // Serial.println(NextTimeToCheck);
         Pin_Timer_Status = digitalRead(Pin_Timer);
         if (Pin_Timer_Status == LOW)
         {
           count++;
           
-          Serial.println("  ID" + ID_Char[ID_timer]  +":"+ String(count));
+         // Serial.println("  ID" + ID_Char[ID_timer]  +":"+ String(count));
      
           is_finished = false;
         }
         else 
         {
           is_finished = true;
-          Serial.println("LISTO");
+         // Serial.println("Y");
         }
       }
     }
     //Se obtiene el conteo actual del timer
-    int SendCount()
-    {
-      //Serial.print("ID02ID12ID32ID42ID52ID62ID72ID82");
-      //Serial.flush();
-     // return (count);
-    }
+
 };
 //Primer juego de timers
 Timer_device TDev1, TDev2, TDev3, TDev4, TDev5, TDev6, TDev7, TDev8, TDev9, TDev10, TDev11;
@@ -63,7 +59,7 @@ Timer_device TDev12, TDev13, TDev14, TDev15, TDev16, TDev17, TDev18, TDev19, TDe
 Timer_device TDevices2[11] = {TDev12, TDev13, TDev14, TDev15, TDev16, TDev17, TDev18, TDev19, TDev20, TDev121, TDev22};
 void setup() {
   Serial.begin(115200);
-  t.every(1000, TDevices1[0].SendCount());
+ 
   for (i = 0; i <= 10; i++)
   {
     TDevices1[i].Initialize_Pin(Group1_PinTimerDev[i]);
@@ -71,8 +67,12 @@ void setup() {
   }
   pinMode(Trigger_Test_Pin_1,INPUT);
   pinMode(Trigger_Test_Pin_2,INPUT);
+  t.every(200, SendCount);
   
 }
+
+
+
 void loop() {
   if(digitalRead(Trigger_Test_Pin_1) == true){
     for(i = 0; i <= 10; i++){
@@ -101,6 +101,19 @@ void loop() {
        
     }
   }
- // t.update();
+ t.update();
   //Serial.print("ASD");
+}
+
+void SendCount()
+{
+  for (i = 0; i <= 10; i++){
+     Msg_To_Android =  Msg_To_Android + ID_Char[i] +  String(TDevices1[i].count);
+  }
+  for (i = 0; i <= 10; i++){
+     Msg_To_Android =  Msg_To_Android + ID_Char[i + 11] +  String(TDevices2[i].count);
+  }
+  //Msg_To_Android = "ASD";
+  Serial.println(String(Msg_To_Android));
+  Msg_To_Android = "";
 }
